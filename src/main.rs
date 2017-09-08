6ce5a468a71b4ad1a6cdc0431e98544109ccaf8c
Christian Dürr <contact@christianduerr.com>
2017-09-08 23:22:44 +0200

    Refactor main.rs
    The first step of a bunch of refactoring changes to make testing easier.
    
    This adds a global `READER` to the main.rs which can be used from
    everywhere without having to pass around parameters. It still allows
    mocking using a Cursor.
    
    A `submatches` method has been added to cut down the main method's line
    length a bit and prevent some useless repetition.
    
    The `error!` macro has been added for printing to stderr with a red text
    and then automatically resetting the text color back to white. This
    hopefully just make the errors in the code a little more readable.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ extern crate clap;
 #[macro_use]
 extern crate error_chain;
 #[macro_use]
+extern crate lazy_static;
+#[macro_use]
 extern crate serde_derive;
 
 extern crate base64;
@@ -30,30 +32,66 @@ mod errors {
     }
 }
 
+use std::sync::{Arc, Mutex};
+use std::process::exit;
 use errors::*;
 use clap::App;
+use std::io;
+
+// Setup a mock for stdin to make testing possible without excessive parameters
+#[cfg(not(test))]
+lazy_static! {
+    static ref READER: Arc<Mutex<io::Stdin>> = {
+        Arc::new(Mutex::new(io::stdin()))
+    };
+}
+
+#[cfg(test)]
+lazy_static! {
+    static ref READER: Arc<Mutex<io::BufRead + Send + Sync>> = {
+        Arc::new(Mutex::new(io::Cursor::new(Vec::new())))
+    };
+}
+
+// Macro for printing red text to stderr
+macro_rules! error {
+    ($x:expr) => {
+        eprintln!("\x1b[0;31;40m{}\x1b[0m\n", $x);
+    };
+}
 
 quick_main!(run);
 fn run() -> Result<()> {
+    // Create config if it does not exist already
     if !config::config_exists() {
-        eprintln!("\x1b[0;31;40mNo config file found.\x1b[0m\n");
+        error!("No config file found");
         config::create_config()?;
     }
 
+    // Parse CLI parameters
     let yaml = load_yaml!("clap.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
+    // Execute subcommnd with CLI parameters
     if let Some(subcommand) = matches.subcommand_name() {
         match subcommand {
-            "add" => add::run(matches.subcommand_matches("add").unwrap())?,
-            "list" => list::run(matches.subcommand_matches("list").unwrap())?,
-            "remove" => remove::run(matches.subcommand_matches("remove").unwrap())?,
-            "update" => update::run(matches.subcommand_matches("update").unwrap())?,
+            "add" => add::run(submatches(&matches, "add"))?,
+            "list" => list::run(submatches(&matches, "list"))?,
+            "remove" => remove::run(submatches(&matches, "remove"))?,
+            "update" => update::run(submatches(&matches, "update"))?,
             _ => (),
         };
     } else {
-        Err("No operation specified (use -h for help)")?;
+        // Complain when no parameters are specified
+        error!("No operation specified (use -h for help)");
+        exit(1);
     }
 
     Ok(())
+}
+
+#[inline]
+fn submatches<'a>(matches: &'a clap::ArgMatches, subcommand: &str) -> &'a clap::ArgMatches<'a> {
+    // This is unwrapped because it's only executed when the subcommand is called
+    matches.subcommand_matches(subcommand).unwrap()
 }
